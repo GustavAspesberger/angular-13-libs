51b13bfb81914ff9ce98abf7830cd48578e6afd2
Gustav Aspesberger <gustav.aspesberger@takealot.com>
2021-12-13 17:16:02 +0200

    added tests

--- npx-enum-presentation-pipe/projects/ngx-enum-presentation-pipe/src/lib/ngx-enum-presentation-pipe.pipe.spec.ts
+++ npx-enum-presentation-pipe/projects/ngx-enum-presentation-pipe/src/lib/ngx-enum-presentation-pipe.pipe.spec.ts
@@ -5,4 +5,61 @@ describe('enumPresentation', () => {
     const pipe = new NgxEnumPresentationPipe();
     expect(pipe).toBeTruthy();
   });
+
+  const testWithExcludedWord = ( input: string, excludedWordInput: string[] | undefined, output: string ) => {
+    const pipe = new NgxEnumPresentationPipe();
+    expect( pipe.transform( input, excludedWordInput ) )
+      .toEqual( output );
+  }
+
+  const testWithoutExcludedWord = ( input: string, output: string ) => {
+    const pipe = new NgxEnumPresentationPipe();
+    expect( pipe.transform( input ) )
+      .toEqual( output );
+  }
+
+  it('Empty input, no explicit excludedWord -> empty string output', () => {
+    testWithoutExcludedWord( '', '')
+  });
+
+  it('WORD input, no explicit excludedWord -> Word output', () => {
+    testWithoutExcludedWord( 'WORD', 'Word')
+  });
+
+  it('Empty input -> empty string output', () => {
+    testWithExcludedWord('', undefined, '');
+  });
+
+  it('Empty input -> empty string output', () => {
+    testWithExcludedWord('', undefined, '');
+  });
+
+  it('2 underscore input -> 2 spaces output', () => {
+    testWithExcludedWord('__', undefined, '  ');
+  });
+
+  it('Single Word (ALL CAPS) input -> Single word (First Letter Capitalised)', () => {
+    testWithExcludedWord('WORD', undefined, 'Word');
+  });
+
+  it('Single Word (no caps) input -> Single word (First Letter Capitalised)', () => {
+    testWithExcludedWord('word', undefined, 'Word');
+  });
+
+  it('Multiple Words (CAPS and Underscore) -> Multiple words ( _ -> space, All words first letter Capitalised )', () => {
+    testWithExcludedWord('TEST_WORD', undefined, 'Test Word');
+  });
+
+  it('Multiple Words (CAPS and space) -> Multiple words ( First word First letter Capitalised )', () => {
+    testWithExcludedWord('TEST WORD', undefined, 'Test word');
+  });
+
+  it('Special Words should be all CAPS', () => {
+    testWithExcludedWord('TEST_WORD', ['word'], 'Test WORD');
+  });
+
+  it('word containing section of special word not presented in all caps', () => {
+    testWithExcludedWord('TEST_WORDS', ['word'], 'Test Words');
+  });
+
 });
