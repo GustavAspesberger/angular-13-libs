fd32cea2e2cd8e91edf7b20e9631fd0a12052b62
Gustav Aspesberger <gustav.aspesberger@takealot.com>
2021-12-13 17:07:27 +0200

    updated testing conf

--- npx-enum-presentation-pipe/projects/ngx-enum-presentation-pipe/karma.conf.js
+++ npx-enum-presentation-pipe/projects/ngx-enum-presentation-pipe/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath('/usr/bin/chromium-browser')
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -10,27 +12,36 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('karma-spec-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       jasmine: {
         // you can add configuration options for Jasmine here
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+        random: false
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false         // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true           // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ngx-enum-presentation-pipe'),
+      dir: require('path').join(__dirname, '../../coverage/enum-presentation'),
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text', file: 'fileCoverage.txt' },
+        { type: 'text-summary', file: 'coverage.txt' }
       ]
+    },
+    specReporter: {
+      maxLogLines: 3,              // limit number of lines logged per test
+      suppressErrorSummary: true,  // do not print error summary
+      suppressFailed: false,       // do not print information about failed tests
+      suppressPassed: false,       // do not print information about passed tests
+      suppressSkipped: true,       // do not print information about skipped tests
+      showSpecTiming: true         // print the time elapsed for each spec
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -38,7 +49,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
